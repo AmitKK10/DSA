0bbc4dde17207f1d56499d8f5d4e6b0274ddcc84
Amit Kiran Kar <141171486+AmitKK10@users.noreply.github.com>
2025-08-16 13:41:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.cpp
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -2,14 +2,21 @@ class Solution {
   public:
     int majorityElement(vector<int>& arr) {
         // code here
+        
         int n = arr.size();
+        
         map<int,int> freq;
-        for(int num : arr)
+        
+        for(int num :arr)
         {
-            freq[num]++;
-            if(freq[num]>n/2)
+           freq[num]++;
+        }
+        
+        for(auto &ans : freq )
+        {
+            if(ans.second >n/2)
             {
-                return num ;
+                return ans.first;
             }
         }
         
